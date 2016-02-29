e754e4502eb28a959c67b42accbf83b2c955e361
Brenna Artman <artmanbc@dukes.jmu.edu>
2016-02-29 17:46:26 -0500

    Create math_spec.js

---
+++ math_spec.js
@@ -0,0 +1,23 @@
+var math = require('../math');
+
+describe("A simple program that does arithmetic", function() {
+  it("can add two numbers", function() {
+    expect(math.add(3,5)).toBe(8);
+    expect(math.add(3.2,5.7)).toBe(8.9);
+    expect(math.add(-3,5)).toBe(2);
+  });
+
+  it("can subtract two numbers", function() {
+  	expect(math.subtract(3,5)).toBe(-2);
+  });
+
+  it("can multiply two numbers", function() {
+	expect(math.multiply(3,5)).toBe(15);	
+	});
+
+  it("can divide two numbers", function() {
+  	expect(math.divide(15,5)).toBe(3);
+  	expect(math.divide(15,0)).toBe("Error! Division by zero.");
+  });
+
+});
